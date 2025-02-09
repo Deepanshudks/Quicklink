7694a7215a916719a71e2b87680c6c50656a28b6
Kuldeep Gaud <140884017+Deepanshudks@users.noreply.github.com>
2025-02-10 01:38:23 +0530

    fix

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -4,21 +4,21 @@ import jwt from 'jsonwebtoken';
 export const authenticate = (req: Request, res: Response, next: NextFunction) => {
   // console.log("token")
   const token = req.header('Authorization')?.split(" ")[1]
-  // console.log("Token::::::::",token)
-  // console.log(req.header('Authorization'))
-  // console.log("token is", token)
+  console.log("Token::::::::",token)
+  console.log(req.header('Authorization'))
+  console.log("token is", token)
   if (!token) {
-    // console.log("token not found")
+    console.log("token not found")
     return res.status(401).json({ message: 'Authentication failed. Token missing.' });
   }
 
   try {
-    // console.log("verifying")
+    console.log("verifying")
     const decoded: any = jwt.verify(token, process.env.JWT_SECRET!);
-    // console.log(decoded)
+    console.log(decoded)
             // @ts-ignore 
     req.userId = decoded.userId;
-    // console.log("done")
+    console.log("done")
     next();
   } catch (error) {
     return res.status(401).json({ message: 'Authentication failed. Invalid token.' });

1463755863a773b40e19badff1bf9cbb2bff438d
Kuldeep Gaud <140884017+Deepanshudks@users.noreply.github.com>
2025-10-25 20:38:31 +0530

    fix

--- backend/src/utils/qrCodeUtil.ts
+++ backend/src/utils/qrCodeUtil.ts
@@ -1,11 +1,10 @@
-import QRCode from 'qrcode';
+import QRCode from "qrcode";
 
-// Function to generate a QR code for a file download link
 export const generateQrCode = async (fileURL: string) => {
   try {
     const qrCode = await QRCode.toDataURL(fileURL);
     return qrCode;
   } catch (error) {
-    throw new Error('Failed to generate QR code.');
+    throw new Error("Failed to generate QR code.");
   }
 };
